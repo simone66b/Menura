e5f6674c7f705a185fe0d16b5e0043d4805cc0fb
Unknown <moreau.cheyenne@gmail.com>
2018-03-16 13:08:14 +1000

    attempted fossil ID
    not working

--- menura/R/example_tree.R
+++ menura/R/example_tree.R
@@ -15,20 +15,82 @@
   edgelabels()
   nodelabels()
   add.scale.bar()
+  
+  
   ##working on manually adding fossils to tree as new branches with length 0
-  tip <- list(edge = matrix(c(2,1),1,2), 
+  tip1 <- list(edge = matrix(c(2,1),1,2), 
               tip.label = "fossil",
               edge.length = 0.0,
               Nnode = 1)
-  class(tip)<- "phylo"
-  ftree <- bind.tree(tr,tip, where = 5)
-  tr$edge[, 1]
-  tr$edge
+  class(tip1)<- "phylo"
+  f1t <- bind.tree(tr,tip1, where = 7, position = 0.1)
+  plot(f1t)
+  nodelabels()
+  tip2 <- list(edge = matrix(c(2,1),1,2), 
+              tip.label = "fossil",
+              edge.length = 0.0,
+              Nnode = 1)
+  class(tip2)<- "phylo"
+  ftree <- bind.tree(f1t,tip2, where = 7, position = 0.1)
+  plot(ftree)
+  nodelabels()
+  
+  ftree$edge.length
+
+  #a way to add multiple fossils given node #, tip.label, edge_length
+  #PROBLEM, when entering a node at a position on the branch, nodes get reset each time
+  bind.tip <- function(tree, tip.label, edge.length = NULL, where = NULL, position = NULL){
+    if(is.null(where)) where <- length(tree$tip.label)+1
+    tip <- list(edge = matrix(c(2,1),1,2), 
+                 tip.label = "fossil",
+                 edge.length = 0.0,
+                 Nnode = 1)
+    class(tip)<- "phylo"
+    obj <- bind.tree(tree, tip, where = where)
+    return(obj)
+  }
+  
+  ###attempt at rerooting from fossils
+  
+  
+  
+  
+  #step through the tree looking for fossils
+  #at each node check both sides edge length, if edge length == 0 and a tip edge, 
+  #add tip to vector "fossils"
+  br_class <- branchClasses(ftree)
+  fossils <- vector(mode = "numeric")
+  
+  #indicates which edges have fossils attached
+  br_zero <- which(ftree$edge.length[] == 0.0)
+  #indicates which tips are fossils
+  f_label <- which(ftree$tip.label[] == "fossil")
+ 
+  n_edges <- Nedge(ftree)
+  
+
+  find_fossils <- function(tr, br_class, e) 
+    if ((tr$edge.length[e] == 0.0) & (br_class[e] == dead)) 
+       return e
+  
+  
+  e = 1
+  n_edges = Nedge(tr)
+  for(e in n_edges){
+    fossil <- find_fossils(ftree, br_class, e)
+  }
+  
+  
+  br_class$brlen.dead
   plot(ftree)
   ftree$edge.length
   edgelabels()
   nodelabels()                                                    
-  tr$edge
+  ftree$edge.length
+  
+  
+  
+  
   # SDE parameters
   # set to size of the length of the edge.length vector
   Nedges <- length(tr$edge.length)  
