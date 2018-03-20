71c698e116354d739c2bb80d06d2755352c460fb
Unknown <moreau.cheyenne@gmail.com>
2018-03-20 14:51:26 +1000

    fossil ID fxn working

---
+++ menura/R/fossil_id.R
@@ -0,0 +1,18 @@
+fossil_id <- function(tr, tr_tips){
+  # identify which branches have length 0
+  br_zero <-  which(ftr$edge.length[] == 0.0) 
+  br_zero
+  #from this set, need to identify which ones are connected to tips
+  is_tip <- ftr$edge[br_zero,2]
+  #provides a set of node values with edge 0, potential tips
+  
+  fossils <- 0
+  #fossils will be added to the tree separately, this means they will have tip values greater than the original tips
+  #and less than the internal notes, the difference in tips in the range they are in
+  for(i in 1:length(is_tip)){
+    if ((is_tip[i] <= Ntip(ftr)) && (is_tip[i] > Ntip(tr))){
+      fossils[i] <- is_tip[i]
+    }
+  } 
+  fossils
+}  
